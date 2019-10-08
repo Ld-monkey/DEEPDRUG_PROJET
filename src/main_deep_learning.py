3e11ae65a8c58f112dc93f16ae744f7763ebbb17
lfourteau91@gmail.com <lfourteau91@gmail.com>
2019-10-08 19:56:36 +0200

    Ajout de dictionnaire associé aux valeurs .npy

--- src/main_deep_learning.py
+++ src/main_deep_learning.py
@@ -2,11 +2,22 @@
 import numpy as np
 
 def read_name_pdb_to_list(links, name_list):
-    ''' Méthode ajoute les noms de pdb dans une liste. '''
+    ''' Method which add all names of pdb into list. '''
     with open(links, "r") as list_pdb:
         for line in list_pdb:
             name_list.append(line.strip())
 
+def create_dictonnary_of_matrix_npy(links, name_list_reduce):
+    ''' Method which return dictionnary from lists of name pdb and
+    associated the correct matrix .npy .'''
+    basic_dict = dict()
+
+    for i in range(0, len(name_list_reduce), 1):
+        numpy_array = np.load(links+name_list_reduce[i]+".npy")
+        control_dict = {name_list_reduce[i] : numpy_array}
+
+    print("Finish !")
+    return basic_dict
 
 if __name__ == "__main__":
     print("DEEP DRUG 3D - 2019 - 2020")
@@ -28,3 +39,35 @@ if __name__ == "__main__":
 
     # Add steroid list.
     read_name_pdb_to_list("../data/steroid_list/steroid.list", steroid_list)
+
+    # 1/10 of lists to reduce data.
+    control_list_reduce = np.random.choice(control_list,
+                                           int(len(control_list)/10),
+                                           replace = False)
+
+    heme_list_reduce = np.random.choice(heme_list,
+                                        int(len(heme_list)/10),
+                                        replace = False)
+    nucleotide_list_reduce = np.random.choice(nucleotide_list,
+                                              int(len(nucleotide_list)/10),
+                                              replace = False)
+
+    steroid_list_reduce = np.random.choice(steroid_list,
+                                           int(len(steroid_list)/10),
+                                            replace = False)
+
+    # Define the path of all numpy matrix.
+    path_deepdrug3D = "../data/deepdrug3d_voxel_data/"
+
+    # Create dictionnarys.
+    control_dict = create_dictonnary_of_matrix_npy(path_deepdrug3D,
+                                                   control_list_reduce)
+
+    heme_dict = create_dictonnary_of_matrix_npy(path_deepdrug3D,
+                                                heme_list_reduce)
+
+    nucleotide_dict = create_dictonnary_of_matrix_npy(path_deepdrug3D,
+                                                      nucleotide_list_reduce)
+
+    steroid_dict = create_dictonnary_of_matrix_npy(path_deepdrug3D,
+                                                   steroid_list_reduce)
